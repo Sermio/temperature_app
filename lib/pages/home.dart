5ce20d41ec9348db9cff4f05cb13d9f8ef65a8de
MT <sergiomt97@gmail.com>
2025-04-02 00:44:38 +0200

    base app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -23,37 +23,130 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        centerTitle: true,
+        title: const Text('Temperatura'),
+        backgroundColor: Colors.blueAccent, // Color de la barra superior
       ),
-      body: StreamBuilder<DatabaseEvent>(
-        stream: _sensorStream,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue.shade700,
+              Colors.blue.shade300
+            ], // Gradiente de azul oscuro a azul claro
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: StreamBuilder<DatabaseEvent>(
+          stream: _sensorStream,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
 
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
 
-          if (snapshot.hasData) {
-            final data = snapshot.data!.snapshot.value as Map<dynamic, dynamic>;
-            final humidity = data['humidity'] ?? 'No data';
-            final temperature = data['temperature'] ?? 'No data';
+            if (snapshot.hasData) {
+              final data =
+                  snapshot.data!.snapshot.value as Map<dynamic, dynamic>;
+              final humidity = data['humidity'] ?? 'No data';
+              final temperature = data['temperature'] ?? 'No data';
 
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Humidity: $humidity'),
-                  Text('Temperature: $temperature'),
-                ],
-              ),
-            );
-          }
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Tarjeta para la temperatura
+                      Card(
+                        color: Colors.white.withOpacity(0.8),
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              const Icon(
+                                Icons.thermostat,
+                                color: Colors.orange,
+                                size: 50,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Temperature',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue.shade700,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                '$temperature °C',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue.shade700,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      // Tarjeta para la humedad
+                      Card(
+                        color: Colors.white.withOpacity(0.8),
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              const Icon(
+                                Icons.water_drop,
+                                color: Colors.blue,
+                                size: 50,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Humidity',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue.shade700,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                '$humidity %',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue.shade700,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
 
-          return const Center(child: Text('No data available'));
-        },
+            return const Center(child: Text('No data available'));
+          },
+        ),
       ),
     );
   }

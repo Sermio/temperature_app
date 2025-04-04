5bc4f4b17e55980712b526680138680ff6fffbb7
MT <sergiomt97@gmail.com>
2025-04-04 21:40:15 +0200

    added humidity soil sensor

--- arduino_code/temp/temp.ino
+++ arduino_code/temp/temp.ino
@@ -9,6 +9,9 @@ ModulinoBuzzer buzzer;
 int frequency = 440;
 int duration = 1000;
 
+int humedadExtraPin = A0;  // Pin donde está conectado el sensor de humedad (puedes usar A1 u otro pin analógico libre)
+int humedadExtra;          // Variable para almacenar el valor de humedad
+
 WiFiServer server(80);
 Firebase fb(REFERENCE_URL);
 
@@ -25,16 +28,30 @@ void setup() {
 
   Modulino.begin();
   thermo.begin();
-  buzzer.begin();c:\Users\Sergio\Desktop\git\temperature_app\arduino_code\temp\secrets.h
+  buzzer.begin();
+
+  // Configurar el pin A1 (o el que hayas elegido) como entrada
+  pinMode(humedadExtraPin, INPUT);
 }
 
 void loop() {
+  Serial.print("-");
 
     float celsius = thermo.getTemperature();
     float humidity = thermo.getHumidity();
 
+    // Leer el valor del sensor ICQUANZX
+    humedadExtra = analogRead(humedadExtraPin);
+
+    // Convertir la lectura analógica del sensor ICQUANZX a un valor de humedad en porcentaje
+    float humedadExtraPorcentaje = map(humedadExtra, 0, 1023, 100, 0); 
+
     fb.setFloat("sensors/temperature", celsius); // Establece el valor de temperatura en Firebase
     fb.setFloat("sensors/humidity", humidity); // Establece el valor de humedad en Firebase
+    fb.setFloat("sensors/humidityExtra", humedadExtraPorcentaje); // Establece el valor de humedad extra en Firebase
+
+    Serial.print("Humedad: ");
+    Serial.println(humedadExtraPin); 
 
     // if(humidity >= 60){
     //   buzzer.tone(frequency, duration);
